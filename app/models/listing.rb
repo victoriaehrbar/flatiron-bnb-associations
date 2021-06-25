561c90dd5c5ebf04372cf9d6dac6115534ec2da4
github username <github email address>
2021-06-24 20:34:06 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,7 @@
-class Listing < ApplicationRecord
+class Listing < ActiveRecord::Base
+    belongs_to :host, :class_name => :User
+    belongs_to :neighborhood
+    has_many :reservations
+    has_many :reviews, through: :reservations
+    has_many :guests, through: :reservations
 end
